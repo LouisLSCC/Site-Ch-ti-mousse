d2686d911ab4d1ed4787f3ceaaf78784cd5e497d
Louis <louis.lesur@protonmail.com>
2021-07-09 10:03:54 +0200

    commitamère

--- compte.php
+++ compte.php
@@ -5,26 +5,46 @@ session_start();
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
+    <link rel="stylesheet" href="styles.css">
     <meta charset="utf-8">
     <title></title>
   </head>
 
   <body>
 
-  <h1>Page de mousaillon</h1>
+    <header>
 
-  <div class="">
+     <div class="titre">
+      <h1>Page de mousaillon</h1>
+     </div>
+      <div id="navbar">
+        <a href="home.html">
+          <button class="tablink" >Accueil</button>
+        </a>
+        <a href="événement.html">
+          <button class="tablink" >Evénement</button>
+        </a>
+        <a href="about.html">
+          <button class="tablink" >About</button>
+        </a>
+        <a href="compte.php">
+          <button class="tablink" onclick="">Compte</button>
+        </a>
+      </div>
 
-  <?php if (isset($_GET["name"])) :
-    $_SESSION['user'] = $_GET["name"];?>
+    </header>
 
-      <p>Nom de mousaillon : <?php echo htmlspecialchars ($_GET["name"]);?></p>
+  <div class="content-box">
+    <?php if (isset($_GET["name"])) :
+      $_SESSION['user'] = $_GET["name"];?>
 
-  <?php else : ?>
+        <p>Nom de mousaillon : <?php echo htmlspecialchars ($_GET["name"]);?></p>
 
-      <p>Nom de mousaillon : Mousaillon fantome</p>
+    <?php else : ?>
 
-  <?php endif ?>
+        <p>Nom de mousaillon : Mousaillon fantome</p>
+
+    <?php endif ?>
   </div>
 
   </body>
